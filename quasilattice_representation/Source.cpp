3a75a4828b8d07f70dcb39705426855b333370ff
so-nakashima <naaso0510@gmail.com>
2016-10-21 10:49:27 +0900

    first commit/ make project file

---
+++ quasilattice_representation/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<math.h>
+#include<map>
+#include<iterator>
+
+#define rep(i,n) for(int i = 0; i != n; i++)
+
+using namespace std;
+
+int main(){
+
+}
